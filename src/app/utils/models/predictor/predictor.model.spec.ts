703b757c47191af927a661d0a9ad50aa69950556
Iván Toromoreno Sarango <ivan.toromoreno@epn.edu.ec>
2020-10-08 17:59:56 -0500

    plate number validation test

--- src/app/utils/models/predictor/predictor.model.spec.ts
+++ src/app/utils/models/predictor/predictor.model.spec.ts
@@ -4,22 +4,30 @@ import Predictor from './predictor';
 
 let predictor: Predictor;
 let plateNumber: string;
+let queryDate: Date;
 
 describe('PredictorModel', () => {
 
   beforeEach(() => {
     plateNumber = 'PDA-1245';
+    queryDate = new Date(2020, 9, 7, 7, 0);
   });
 
-  it('should return false', () => {
-    const queryDate = new Date(2020, 9, 7, 7, 0);
+  it('should return false when cannot be on the road', () => {
+    queryDate = new Date(2020, 9, 7, 7, 0);
     predictor = new Predictor(plateNumber, queryDate);
     expect(predictor.getPrediction()).toBeFalsy();
   });
 
-  it('should return true', () => {
-    const queryDate = new Date(2020, 9, 7, 6, 59);
+  it('should return true when can be on the road', () => {
+    queryDate = new Date(2020, 9, 7, 6, 59);
     predictor = new Predictor(plateNumber, queryDate);
     expect(predictor.getPrediction()).toBeTruthy();
+  });
+
+  it('should return false when plate number is empty', () => {
+    plateNumber = '';
+    predictor = new Predictor(plateNumber, queryDate);
+    expect(predictor.getPrediction()).toBeFalsy();
   });
 });
