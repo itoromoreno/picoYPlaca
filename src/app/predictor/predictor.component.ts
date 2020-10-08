86a44ac7410d594362d38d7fe7c842dd313a09bd
Iván Toromoreno Sarango <ivan.toromoreno@epn.edu.ec>
2020-10-08 14:13:54 -0500

    Implementación pantalla de ingreso de datos

---
+++ src/app/predictor/predictor.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-predictor',
+  templateUrl: './predictor.component.html',
+  styleUrls: ['./predictor.component.css']
+})
+export class PredictorComponent implements OnInit {
+
+  success = true;
+
+  predictorForm
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
