cd7030cc43572943b62b7fe2495250a79cfdc794
Iván Toromoreno Sarango <ivan.toromoreno@epn.edu.ec>
2020-10-08 19:08:08 -0500

    predictor response interface implementation

--- src/app/predictor/predictor.component.ts
+++ src/app/predictor/predictor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import Predictor from '../utils/models/predictor/predictor';
+import Response from '../utils/models/response/response';
 
 @Component({
   selector: 'app-predictor',
@@ -11,6 +12,7 @@ export class PredictorComponent implements OnInit {
 
   success = true;
   predictor: Predictor;
+  predictorResponse: Response;
 
   predictorForm = new FormGroup({
     plateNumber: new FormControl(''),
@@ -19,6 +21,21 @@ export class PredictorComponent implements OnInit {
   });
 
   submit() {
+    try {
+      this.getFormInfo();
+    } catch (err) {
+      this.predictorResponse = {
+        success: false,
+        error: {
+          code: 1,
+          errorMessage: 'Revisa los datos ingresados'
+        }
+      }
+    }
+    this.predictorResponse = this.predictor.getPrediction();
+  }
+
+  getFormInfo() {
     const predictorFormValue = this.predictorForm.value;
     const plateNumber = predictorFormValue.plateNumber;
     const [year, month, day] = predictorFormValue.queryDate.split('-');
